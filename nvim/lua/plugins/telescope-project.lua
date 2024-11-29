79525a3d16ba0d866962f25f59e1592fcdde92b0
czheo <1692400+czheo@users.noreply.github.com>
2024-11-29 13:21:00 -0800

    change telescope project shortcut to <leader>p

--- nvim/lua/plugins/telescope-project.lua
+++ nvim/lua/plugins/telescope-project.lua
@@ -2,6 +2,6 @@ return {
   "nvim-telescope/telescope-project.nvim",
   init = function()
     require("telescope").load_extension("project")
-    vim.keymap.set("n", "<leader>fp", "<cmd>Telescope project<cr>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>p", "<cmd>Telescope project<cr>", { noremap = true, silent = true })
   end,
 }
