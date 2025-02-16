0dcac9ca07f7dde910b31a64d434b2f563b9c8bf
czheo <1692400+czheo@users.noreply.github.com>
2025-02-15 17:13:09 -0800

    update work cli

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -4,7 +4,8 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    provider = "openai",
+    -- provider = "openai",
+    provider = "claude",
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
