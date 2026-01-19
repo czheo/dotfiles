03df02843080eaa397d80774195b3708311cfce5
czheo <1692400+czheo@users.noreply.github.com>
2026-01-18 17:44:14 -0800

    update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,9 +5,3 @@
 local map = vim.keymap.set
 
 map("t", "<c-l>", "<nop>")
-
--- Keymaps to move among splits in insert mode
-map("i", "<C-h>", "<C-\\><C-N><C-w>h", { desc = "Move to left split" })
-map("i", "<C-j>", "<C-\\><C-N><C-w>j", { desc = "Move to below split" })
-map("i", "<C-k>", "<C-\\><C-N><C-w>k", { desc = "Move to above split" })
-map("i", "<C-l>", "<C-\\><C-N><C-w>l", { desc = "Move to right split" })
